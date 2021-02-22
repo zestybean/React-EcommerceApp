0bf1090610e75cb7280236f446c007083ddaf4e9
Beandip <brando.lugo@gmail.com>
2021-02-21 21:17:48 -0600

    Added the first product views and components

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+import useStyles from "./styles";
+
+//Destructure the product
+const Product = ({ product }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image=""
+        title={product.name}
+      ></CardMedia>
+      <CardContent>
+        <div className={classes.CardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart"></IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
