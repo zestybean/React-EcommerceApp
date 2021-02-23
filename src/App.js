5b512b08d8b4aeaafe06b81350e8f48023a65c40
Beandip <brando.lugo@gmail.com>
2021-02-23 13:44:57 -0600

    Linking commercejs and project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import { commerce } from "./lib/commerce";
 import { Products, Navbar } from "./components";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  //Fetch the response form the server async
+  const fetchProducts = async () => {
+    //Returns a promise
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
       <Products />
