49646f319543b68f071470a63fa5bf192f26bef1
Beandip <brando.lugo@gmail.com>
2021-03-05 16:36:29 -0600

    Added some funcitonality to the cart, to remove and add quanttities of items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,7 +6,12 @@ import { Link } from "react-router-dom";
 import useStyles from "./styles";
 import CartItem from "./CartItem/CartItem";
 
-const Cart = ({ cart }) => {
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyingCart,
+}) => {
   const classes = useStyles();
 
   const EmptyCart = () => (
@@ -23,7 +28,11 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item} />
+            <CartItem
+              item={item}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
           </Grid>
         ))}
       </Grid>
@@ -38,6 +47,7 @@ const Cart = ({ cart }) => {
             type="button"
             variant="contained"
             color="secondary"
+            onClick={handleEmptyingCart}
           >
             Empty Cart
           </Button>
